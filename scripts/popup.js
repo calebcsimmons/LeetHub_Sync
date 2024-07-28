3ee449f5bf46c5a22e01f424deb37c92b92bee13
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-07-28 11:56:29 -0500

    Added checkboxstate to keep usersettings

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,11 +5,30 @@ function displayMessage(message, color = 'black') {
     messageElement.style.color = color;
 }
 
-// Auto Submit
+// Auto Submit Checkbox
 document.getElementById('auto-submit-checkbox').addEventListener('change', (event) => {
-    const isChecked = event.target.checked;
-    chrome.storage.local.set({ autoSubmit: isChecked });
+    saveCheckboxState(event.target.checked);
 });
+
+// Set initial checkbox state
+setCheckboxState();
+
+// Save checkbox state
+function saveCheckboxState(isChecked) {
+    chrome.storage.sync.set({ autoSubmit: isChecked }, () => {
+        console.log('Auto Submit state saved:', isChecked);
+    });
+}
+
+// Retrieve and set checkbox state
+function setCheckboxState() {
+    chrome.storage.sync.get(['autoSubmit'], (result) => {
+        const autoSubmitCheckbox = document.getElementById('auto-submit-checkbox');
+        if (result.autoSubmit !== undefined) {
+            autoSubmitCheckbox.checked = result.autoSubmit;
+        }
+    });
+}
 
 // Sync Last Submission
 document.getElementById('sync-last-submission-button').addEventListener('click', () => {
